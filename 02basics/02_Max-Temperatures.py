0b3cd29976116d7b44caf0c59b3a1b4c732d08e1
tonirilix <jesusmontes.mx@hotmail.com>
2016-04-30 14:45:18 -0500

    Add file to get max temperatures

--- 02basics/02_Max-Temperatures.py
+++ 02basics/02_Max-Temperatures.py
@@ -1,6 +1,6 @@
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setMaster("local").setAppName("MinTemperatures")
+conf = SparkConf().setMaster("local").setAppName("MaxTemperatures")
 sc = SparkContext(conf = conf)
 
 def parseLine(line):
@@ -12,10 +12,10 @@ def parseLine(line):
 
 lines = sc.textFile("02_1800.csv")
 parsedLines = lines.map(parseLine)
-minTemps = parsedLines.filter(lambda x: "TMIN" in x[1])
-stationTemps = minTemps.map(lambda x: (x[0], x[2]))
-minTemps = stationTemps.reduceByKey(lambda x, y: min(x,y))
-results = minTemps.collect()
+maxTemps = parsedLines.filter(lambda x: "TMAX" in x[1])
+stationTemps = maxTemps.map(lambda x: (x[0], x[2]))
+maxTemps = stationTemps.reduceByKey(lambda x, y: max(x,y))
+results = maxTemps.collect()
 
 for result in results:
     print result[0] + "\t{:.2f}F".format(result[1])
