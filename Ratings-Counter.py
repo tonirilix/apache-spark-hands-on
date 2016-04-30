d42b826ea6105956511cfb8f5e8d13b61f7c8033
tonirilix <jesusmontes.mx@hotmail.com>
2016-04-30 13:14:33 -0500

    Test to make file run in IDE

--- Ratings-Counter.py
+++ Ratings-Counter.py
@@ -1,3 +1,13 @@
+# import os
+# import sys
+#
+# # Path for spark source folder
+# os.environ['SPARK_HOME'] = "/usr/local/Cellar/apache-spark/1.6.1"
+#
+# # Append pyspark  to Python Path
+# sys.path.append("/usr/local/Cellar/apache-spark/1.6.1/libexec/python")
+# sys.path.append("/usr/local/Cellar/apache-spark/1.6.1/libexec/python/lib/py4j-0.9-src.zip")
+
 from pyspark import SparkConf, SparkContext
 import collections
 
