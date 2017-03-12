53dddc3d8794acc44fcd20606d9c8f28f80ec06c
tonirilix <jesusmontes.mx@hotmail.com>
2017-03-11 20:40:30 -0600

    Add comments

--- 02basics/01_Friends-By-Age.py
+++ 02basics/01_Friends-By-Age.py
@@ -10,13 +10,26 @@ def parseLine(line):
     numFriends = int(fields[3])
     return (age, numFriends)
 
+# We get all the data
 lines = sc.textFile("01_fakefriends.csv")
+
+# Extract age and num of friends as a list of tuples
 rdd = lines.map(parseLine)
+
+# First to every value we create a key pair of (value, 1)
+# Then we reduce by age and create a key pair of (sum(value), sum (1)).
+# With that we will know the sum of friends per same age to then get the average
 totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+
+# we get the average by mapping new values
 averagesByAge = totalsByAge.mapValues(lambda x: x[0] / x[1])
+
+# get the last modified list
 results = averagesByAge.collect()
 
-sortedResults = collections.OrderedDict(sorted(results, key=lambda t: t[1]))
+# Sort data by keys
+sortedResults = collections.OrderedDict(sorted(results, key=lambda t: t[0]))
 
+# Loop data to print it
 for result in sortedResults.iteritems():
     print result
